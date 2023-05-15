1087901a24c796e2ab3acfd94541b3c7dfdba007
Mo Amir Khan <104264342+AmirKhancoder@users.noreply.github.com>
2023-05-15 11:13:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,2 @@
-let complexArray = [
-  [
-    {
-      one: 1,
-      two: 2
-    },
-    {
-      three: 3,
-      four: 4
-    }
-  ],
-  [
-    {
-      a: "a",
-      b: "b"
-    },
-    {
-      c: "c",
-      d: "d"
-    }
-  ]
-];
-console.log(complexArrayArray.length);
+let simpleArray = ['one', 2, 'three', true, false, undefined, null];
+console.log(simpleArray.length);
